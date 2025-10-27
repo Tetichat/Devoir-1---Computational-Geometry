8e9df4d7cf082780e1331bcf043b0a6e06375cf0
Abel Gérard <abel.gerard@student.uclouvain.be>
2025-10-27 09:54:25 +0100

    Revert "Revert"
    This reverts commit 5eb9650f34f2dcaffc5c3c2ed729186164f0758e.

--- src/visualize.c
+++ src/visualize.c
@@ -152,12 +152,7 @@ void draw_init(DrawCtx *ctx, MyMesh *mesh) {
         .window_width = 800, .window_height = 600,
         .min_x = min_x, .min_y = min_y, .max_x = max_x, .max_y = max_y,
         .mesh = mesh,
-        .zoom = 1.,
-        .voronoi_button = {
-            .label = CLAY_STRING_CONST("Voronoi"),
-            .text = CLAY_STRING_CONST("Draw Voronoi"),
-            .on = 0,
-        }
+        .zoom = 1.
     };
 }
 
@@ -186,15 +181,11 @@ void draw_handle_user(DrawCtx *ctx) {
     if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
         Vector2 mousePos = GetMousePosition();
         double x = mousePos.x, y = mousePos.y;
-
-        if (y >= ctx->header_height) { // Clicked below header
-            viewportToGeo(ctx, x, y, &x, &y);
-            
-            Vertex newVertex = {x, y}; 
-            _newInsert = 1;
-
-            printf("New vertex at (%f, %f)\n", newVertex.x, newVertex.y);
-        }
+        viewportToGeo(ctx, x, y, &x, &y);
+        
+        Vertex newVertex = {x, y}; 
+        _newInsert = 1;
+        //TODO: 
         
     // Press the middle mouse button to pan around
     } else if (IsMouseButtonDown(MOUSE_BUTTON_MIDDLE)) {
@@ -218,10 +209,10 @@ void draw_handle_user(DrawCtx *ctx) {
         ctx->zoom = 1.;
     } 
     else if (IsKeyPressed(KEY_H)) {
-        _headerToggle = !_headerToggle; 
+        _headerToggle = 1 - _headerToggle; 
     }
     else if (IsKeyPressed(KEY_V)) { // TODO: Transform into checkbox with the triangulation
-        ctx->voronoi_button.on = !ctx->voronoi_button.on; 
+        _voronoiToggle = 1 - _voronoiToggle; 
     }
 }
 
@@ -230,7 +221,7 @@ void render_shortcuts_grid(int headerAlpha) {
         CLAY_STRING_CONST("Esc - Quit"),
         CLAY_STRING_CONST("C - Recenter"),
         CLAY_STRING_CONST("H - Toggle header"),
-        CLAY_STRING_CONST("V - Draw Voronoi"),
+        CLAY_STRING_CONST("V - Toggle Voronoi"),
         // New shortcuts go here...
     };
 
@@ -272,59 +263,9 @@ void render_shortcuts_grid(int headerAlpha) {
     }
 }
 
-void render_button(Button *button) {
-    uint32_t id = (uint32_t)(uintptr_t)button;
-
-    CLAY({
-        CLAY_IDI("_Button", id),
-        .layout = {
-            .padding = 3,
-            .childGap = 5,
-        },
-        .backgroundColor = APP_VVDARK,
-    }) {
-
-        CLAY_TEXT(button->text, CLAY_TEXT_CONFIG({
-            .fontId = FONT_BODY_INDEX,
-            .fontSize = 20,
-            .textColor = APP_PALE_PURPLE,
-            .textAlignment = CLAY_TEXT_ALIGN_CENTER,
-        }));
-
-        CLAY({
-            .id = CLAY_IDI("_ButtonInner", id),
-            .layout = {
-                .sizing = {
-                    .width = CLAY_SIZING_FIXED(20),
-                    .height = CLAY_SIZING_FIXED(20),
-                },
-            },
-            .cornerRadius = CLAY_CORNER_RADIUS(3),
-            .border = { 
-                .color = APP_PURPLE,
-                .width = BORDER_WIDTH_ALL(2), 
-            },
-        }) {
-    
-            if (Clay_Hovered() && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-                button->on = !button->on;
-    
-            if (button->on) {
-                CLAY({
-                    .id = CLAY_IDI("_ButtonInnerContent", id),
-                    .layout = {
-                        .sizing = sizing_expand,
-                    },
-                    .backgroundColor = APP_PALE_PURPLE,
-                    .cornerRadius = CLAY_CORNER_RADIUS(3),
-                }) {};
-            }
-        };
-    }
-}
-
 
 uint8_t _headerToggle = 1;
+uint8_t _voronoiToggle = 0;
 uint8_t _newInsert = 1;
 
 int main(int argc, char* argv[]) {
@@ -434,24 +375,6 @@ int main(int argc, char* argv[]) {
                     if (_headerToggle)
                         render_shortcuts_grid(headerAlpha);
                 }
-
-                CLAY({
-                    ID("HeaderPadding"),
-                    .layout = {
-                        .sizing = sizing_expand,
-                    }
-                }) {}
-
-                CLAY({
-                    ID("HeaderButtons"),
-                    .layout = {
-                        .padding = CLAY_PADDING_ALL(5),
-                    },
-                }) {
-
-                    if (_headerToggle)
-                        render_button(&ctx.voronoi_button);
-                }
             }
         }
  
@@ -521,7 +444,7 @@ int main(int argc, char* argv[]) {
         }
 
         // Drawing the Voronoi diagram 
-        if (ctx.voronoi_button.on) {
+        if (_voronoiToggle) {
             for (int face_id = 0; face_id < mesh.num_faces; face_id++) {
                 Vertex v = adj_verts[face_id];
                 draw_vertex(&ctx, v, 3, CLAY_COLOR_TO_RAYLIB_COLOR(APP_FADED_RED));
@@ -540,12 +463,6 @@ int main(int argc, char* argv[]) {
         Clay_Raylib_Render(renderCommands, fonts);
 
         EndDrawing();
-
-
-        // Update header height in ctx
-        if (!_headerToggle) ctx.header_height = 0;
-        else
-            ctx.header_height = (int)Clay_GetElementData(CLAY_ID("Header")).boundingBox.height;
 
         draw_handle_user(&ctx);
     }
--- src/visualize.h
+++ src/visualize.h
@@ -4,31 +4,22 @@
 #include <raylib.h>
 #include <stdint.h>
 
-#include "clay.h"
 #include "functions.h"
 
-
-typedef struct Button {
-    Clay_String label;
-    Clay_String text;
-    uint8_t on;
-} Button;
 
 typedef struct DrawCtx {
     MyMesh *mesh;
     
     int window_width, window_height;
     double min_x, min_y, max_x, max_y;
-    int header_height; // If clicking in this area, ignore for adding points
 
     int panning_x, panning_y;
     
     float zoom;
-
-    Button voronoi_button;
 } DrawCtx;
 
 extern uint8_t _headerToggle;
+extern uint8_t _voronoiToggle;
 extern uint8_t _newInsert;
 
 void draw_init(DrawCtx *ctx, MyMesh *mesh);
